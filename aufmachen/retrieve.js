132ad155d7d7dca67b854422639875a925ea7085
Frederik De Bleser <frederik@burocrazy.com>
2011-08-07 12:06:25 -0400

    Retrieve.js now uses the IE 8 user agent string.

--- aufmachen/retrieve.js
+++ aufmachen/retrieve.js
@@ -2,6 +2,10 @@
 // Note that only the content of the <body> element is returned.
 var url = phantom.args[0];
 var page = new WebPage();
+page.settings = {}
+page.settings.loadImages = false;
+page.settings.loadPlugins = false;
+page.settings.userAgent = 'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1; .NET CLR 3.0.04320)';
 page.open(url);
 page.onLoadFinished = function(status) {
   if (status !== 'success') {
