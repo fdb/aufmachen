719be2a7b273f9597310d99390b4d639e94331ae
Frederik De Bleser <frederik@burocrazy.com>
2011-08-07 10:45:38 -0400

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='aufmachen',
+    version='0.1-dev',
+    url='http://github.com/fdb/aufmachen',
+    license='BSD',
+    author='Frederik & Jan De Bleser',
+    description='Turns a website\'s HTML into nice, clean objects.',
+    packages=['aufmachen'],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
+
